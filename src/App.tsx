27b5a263b932f39bd7ade649bf0f39ba53238b3a
MohammadNH1 <mustafasheikh28864@gmail.com>
2024-12-18 10:43:44 +0600

    useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-const initialState = {
-  name:'',
-  email:''
-}
+
 function App() {
-  const [data,setData] = useState(initialState)
-  const handleChange =(e:any)=>{
-    setData({...data,
-      [e.target.name]:e.target.value})
-  }
-  const handleSubmit=(e:any)=>{
-    e.preventDefault()
-    console.log('data',data)
-    setData({name:'',email:''})
-  }
+ const [counter,setCounter] = useState<number>(0)
+
+ const handleClick = ()=>{
+  setCounter((prev)=>prev+1)
+ }
+useEffect(()=>{
+  if(counter===4) console.log('hello word')
+},[counter])
+
+
+
   return (
    <>
    <h4>Hello world</h4>
-   <form onSubmit={handleSubmit}>
-    <input type="text" name="name" value={data.name} onChange={handleChange} placeholder="Enter your name"/>
-    <input type="text" name="email" value={data.email} onChange={handleChange} placeholder="Enter your password" />
-    <button  type="submit">Submit</button>
-   </form>
+   <p>{counter}</p>
+   <button onClick={handleClick}>increment</button>
+   
    </>
   )
 }
