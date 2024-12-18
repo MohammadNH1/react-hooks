8cd187ee59b5202b3d293170cec3eb8ba54daf87
MohammadNH1 <mustafasheikh28864@gmail.com>
2024-12-18 15:54:37 +0600

    useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import { Counter } from "./reducer/counter"
+import { useEffect, useRef, useState } from "react"
+// https://www.robinwieruch.de/typescript-react-useref/
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(()=>{
+    if(inputRef.current){
+      inputRef.current.focus()
+    }
+
+  },[])
+
+  type counterRefType= {
+    increament:number,
+    decreament:number
+  }
+  const btnRef = useRef<counterRefType>({
+    increament:0,
+    decreament:0
+  })
+
+  const [count,setCount] = useState<number>(0)
+
+  const handleIncreament = ()=>{
+    btnRef.current.increament++;
+    setCount(count+1);
+  }
+  const handleDecreament = () =>{
+    btnRef.current.decreament++;
+    setCount(count-1)
+  }
   return (
    <>
-   <Counter/>
-  
+   <input type="text" ref={inputRef} placeholder="Enter your name..."/> <br />
+   <p>count: {count}</p>
+   <button onClick={handleIncreament}>+</button>
+   <button onClick={handleDecreament}>-</button> <br />
+   <div> increament cliked {btnRef.current.increament} times</div> 
+   <div> decreament clicked {btnRef.current.decreament} times</div>
    </>
   )
 }
